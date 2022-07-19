cbf87123cd0d4d1ed8f878a4427df78c51b77fe3
Cal Peyser <cpeyser@google.com>
2022-07-19 14:57:33 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+from distutils.core import setup
+setup(name='svcca',
+      version='1.0',
+      py_modules=['pwcca', 'numpy_pls', 'numpy_pca', 'cca_core'],
+)
+
